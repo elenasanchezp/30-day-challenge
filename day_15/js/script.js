209182633ea955731e65a178744394765c9b869b
elenasanchezp <elena.sanchez.p@outlook.com>
2017-11-05 19:50:49 +0000

    day 15 and day 16 files

---
+++ day_15/js/script.js
@@ -0,0 +1,53 @@
+
+
+window.onload = function(e){ 
+
+ 	// Get elements
+	const addItems = document.querySelector('.add-items'); 
+	const itemsList = document.querySelector('.plates');
+	const items = JSON.parse(localStorage.getItem('items')) || [];
+
+	function addItem(e){
+		e.preventDefault();
+
+		const text = (this.querySelector('[name=item]')).value;
+		const item = {
+			text: text, 
+			done: false
+		}
+		
+		items.push(item);
+		populateList(items, itemsList);
+
+		localStorage.setItem('items', JSON.stringify(items));
+		this.reset();
+	}
+
+	function toggleDone(e){
+		if(!e.target.matches('input')) return;
+
+		const el = e.target; 
+		const index = el.dataset.index;
+		items[index].done = !items[index].done;  
+		localStorage.setItem('items', JSON.stringify(items));
+		populateList(items, itemsList);
+	}
+
+	function populateList(plates = [], platesList){
+		platesList.innerHTML =  plates.map((plate, i) => {
+			return 	`
+				<li>
+					<input type="checkbox" data-index=${i} id="item${i}" ${plate.done ? 'checked' : ''} />
+					<label for="">${plate.text}</label>
+				</li>
+			`;
+		}).join(''); 
+	}
+
+	// Hook up the event listeners 
+	addItems.addEventListener('submit', addItem); 
+	itemsList.addEventListener('click', toggleDone); 
+	
+	populateList(items, itemsList);
+}
+
